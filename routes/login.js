25d3eca25a064c62383e6645350cd177255f7bb8
kaquino1 <59567712+kaquino1@users.noreply.github.com>
2022-01-28 21:45:08 -0500

    refactoring

--- routes/login.js
+++ routes/login.js
@@ -4,10 +4,14 @@ var passport = require('passport');
 const router = express.Router();
 
 // REDIRECT TO AUTH0 LOGIN THEN REDIRECT TO CALLBACK
-router.get('/', passport.authenticate('auth0', {
+router.get(
+  '/',
+  passport.authenticate('auth0', {
     scope: 'openid email profile'
-}), function (req, res) {
+  }),
+  (req, res) => {
     res.redirect('/');
-});
+  }
+);
 
 module.exports = router;
