16e4c09ab2186b8f39d640523cb1b586b9dc1f9b
kaquino1 <59567712+kaquino1@users.noreply.github.com>
2022-01-28 21:55:23 -0500

    code formatting

--- routes/users.js
+++ routes/users.js
@@ -6,17 +6,16 @@ const ds = require('../services/datastore');
 const datastore = ds.datastore;
 const USER = 'User';
 
-
 const get_users = async () => {
-    const query = datastore.createQuery(USER);
-    const users = await datastore.runQuery(query);
-    return users[0].map(ds.userFromDatastore);
+  const query = datastore.createQuery(USER);
+  const users = await datastore.runQuery(query);
+  return users[0].map(ds.userFromDatastore);
 };
 
 router.get('/', async (req, res) => {
-    const users = await get_users();
-    res.status(200).json(users);
-    return;
-})
+  const users = await get_users();
+  res.status(200).json(users);
+  return;
+});
 
 module.exports = router;
